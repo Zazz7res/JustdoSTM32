1d7f015207c22b863b464f1552b6b5153cc7c4e4
Zazz7res <harrylee_x@outlook.com>
2026-07-09 10:25:59 +0800

    git add

--- LED_Blink/cmake/arm-none-eabi-gcc.cmake
+++ LED_Blink/cmake/arm-none-eabi-gcc.cmake
@@ -12,3 +12,16 @@ message(STATUS "Toolchain path: ${TOOLCHAIN_PATH}")
 
 # 指定编译器
 
+set(CMAKE_C_COMPILER        ${TOOLCHAIN_PATH}/arm-none-eabi-gcc) 
+set(CMAKE_CXX_COMPILER      ${TOOLCHAIN_PATH}/arm-none-eabi-g++) 
+set(CMAKE_ASM_COMPILER      ${TOOLCHAIN_PATH}/arm-none-eabi-gcc)
+set(CMAKE_OBJCOPY           ${TOOLCHAIN_PATH}/arm-none-eabi-objcopy) 
+set(CMAKE_SIZE_UTIL         ${TOOLCHAIN_PATH}/arm-none-eabi-size)  
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC LIBRARY)  
+
+# 基本编译选项
+set(COMMON_FLAGS "-mcpu=cortex-m3 -mthumb -mfloat-abi=soft") 
+set(CMAKE_C_FLAGS "${COMMON_FLAGS}" CACHE STRING "" FORCE) 
+set(CMAKE_CXX_FLAGS "${COMMON_FLAGS}" CACHE STRING "" FORCE) 
+set(CMAKE_ASM_FLAGS "${COMMON_FLAGS}" -x assembler-with-cpp" CACHE STRING "" FORCE)
